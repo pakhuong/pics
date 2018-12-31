702ccfe989dfcb67377a7c02b5264e5f0bc6f05a
pakhuong <anhkhuong1602@gmail.com>
2018-12-31 19:57:04 +0700

    Initialize SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import SearchBar from './SearchBar';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                App
+                <SearchBar />
             </div>
         );
     }
