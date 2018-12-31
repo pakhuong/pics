6caca26243ab0de358cbcf7d8a12e40785bad586
pakhuong <anhkhuong1602@gmail.com>
2018-12-31 20:04:36 +0700

    Add some styling for the app

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar';
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar />
             </div>
         );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     render() {
         return (
-            <div>
-                <input type="text" />
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" />
+                    </div>
+                </form>
             </div>
         );
     }
