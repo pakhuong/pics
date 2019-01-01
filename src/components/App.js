7ecec4e84e7a4c2abca9790b47bd7729470e5376
pakhuong <anhkhuong1602@gmail.com>
2019-01-01 15:56:37 +0700

    Add axios and fetch images from unsplash.com based on search term

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 import SearchBar from './SearchBar';
 
 class App extends Component {
     onSearchSubmit = term => {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID f5b6b0b12a05d66de3e09ea9ef63bfb1d5376acf34979dfe5047afdc36ca151f'
+            }
+        });
     }; 
 
     render() {
